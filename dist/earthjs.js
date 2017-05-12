21b85d2eaf6a6018401d8ce5720615735ba4eb24
wharsojo <wharsojo@gmail.com>
2017-05-13 03:44:28 +0800

    enhaced canvasPlugin

--- dist/earthjs.js
+++ dist/earthjs.js
@@ -344,28 +344,30 @@ var wheelZoomPlugin = function() {
 
 // Bo Ericsson’s Block http://bl.ocks.org/boeric/aa80b0048b7e39dd71c8fbe958d1b1d4
 var canvasPlugin = function() {
-    var _ = {svg:null, select: null};
+    var _ = {canvas: null, path: null, q: null};
 
     function svgAddCanvas() {
-        _.svg.selectAll('.canvas').remove();
         if (this._.options.showCanvas) {
-            var fObject = _.svg.append("g").attr("class","canvas").append("foreignObject")
+            if (!_.canvas) {
+                var fObject = this._.svg.append("g").attr("class","canvas").append("foreignObject")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("width", this._.options.width)
+                .attr("height", this._.options.height);
+                var fBody = fObject.append("xhtml:body")
+                .style("margin", "0px")
+                .style("padding", "0px")
+                .style("background-color", "none")
+                .style("width", this._.options.width + "px")
+                .style("height", this._.options.height + "px");
+                _.canvas = fBody.append("canvas");
+            }
+            _.canvas
             .attr("x", 0)
             .attr("y", 0)
             .attr("width", this._.options.width)
             .attr("height", this._.options.height);
-            var fBody = fObject.append("xhtml:body")
-            .style("margin", "0px")
-            .style("padding", "0px")
-            .style("background-color", "none")
-            .style("width", this._.options.width + "px")
-            .style("height", this._.options.height + "px");
-            this._.canvas = fBody.append("canvas")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("width", this._.options.width)
-            .attr("height", this._.options.height);
-            return this._.canvas;
+            return _.canvas;
         }
     }
 
@@ -374,28 +376,30 @@ var canvasPlugin = function() {
         onInit() {
             this.svgAddCanvas = svgAddCanvas;
             this._.options.showCanvas = true;
-            _.svg = this._.svg;
+            _.path = d3.geoPath().projection(this._.proj);
         },
         onRefresh() {
             var width = this._.options.width,
                 height= this._.options.height;
-            this._.svg.each(function() {
-                var context = this.getElementsByTagName('canvas')[0].getContext("2d");
-                context.clearRect(0, 0, width, height);
+            _.canvas.each(function() {
+                this.getContext("2d").clearRect(0, 0, width, height);
             });
         },
-        select(slc) {
-            _.svg = d3.selectAll(slc);
-            _.select = slc;
-            return _.svg;
+        selectAll(q) {
+            if (q) {
+                _.q = q;
+                _.canvas = d3.selectAll(q);
+            }
+            return _.canvas;
         },
         render(fn) {
-            var _this = this;
-            var cpath = d3.geoPath().projection(this._.proj);
-            this._.svg.each(function() {
-                var context = this.getElementsByTagName('canvas')[0].getContext("2d");
-                fn.call(_this, context, cpath.context(context));
-            });
+            if (this._.options.showCanvas) {
+                var _this = this;
+                _.canvas.each(function() {
+                    var context = this.getContext("2d");
+                    fn.call(_this, context, _.path.context(context));
+                });
+            }
         }
     }
 };
