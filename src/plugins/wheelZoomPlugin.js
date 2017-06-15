3df1dea3b8a76610c03a89f2edbb0d2854056665
wharsojo <wharsojo@gmail.com>
2017-06-15 10:00:06 +0800

    update wheelZoomPlugin

--- src/plugins/wheelZoomPlugin.js
+++ src/plugins/wheelZoomPlugin.js
@@ -1,14 +1,36 @@
 export default function() {
     /*eslint no-console: 0 */
+    const _ = {svg:null, q: null, sync: []};
+
+    function zoomSetup() {
+        const __ = this._;
+        _.svg.on('wheel', function() {
+            let y = d3.event.deltaY+__.proj.scale();
+            y = (y<20 ? 20 : (y>999 ? 1000 : y));
+            __.scale(y);
+            _.sync.forEach(function(g) {
+                g._.scale.call(g, y);
+            })
+        });
+    }
+
     return {
         name: 'wheelZoomPlugin',
         onInit() {
-            const __ = this._;
-            __.svg.on('wheel', function() {
-                let y = d3.event.deltaY+__.proj.scale();
-                y = (y<20 ? 20 : (y>999 ? 1000 : y));
-                __.scale(y);
-            });
+            _.svg = this._.svg;
+            zoomSetup.call(this);
+        },
+        selectAll(q) {
+            if (q) {
+                _.q = q;
+                _.svg.on('wheel', null);
+                _.svg = d3.selectAll(q);
+                zoomSetup.call(this);
+            }
+            return _.svg;
+        },
+        sync(arr) {
+            _.sync = arr;
         }
     }
 }
