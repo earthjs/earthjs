d716d67ee61a383a065f5daad39c3f3e078a9415
Widi Harsojo <wharsojo@gmail.com>
2017-06-29 15:55:48 +0800

    update hoverCanvas.js

--- src/plugins/hoverCanvas.js
+++ src/plugins/hoverCanvas.js
@@ -46,10 +46,12 @@ export default function() {
             _.dot = null;
             _.mouse = mouse;
             _.country = null;
-            _.addSelectCircleEventKeys.forEach(k => {
-                _.dot = _.addSelectCircleEvent[k].call(this, _.mouse, pos);
-            });
-            if (_.countries && !_.dot) {
+            if (__.options.showDots) {
+                _.addSelectCircleEventKeys.forEach(k => {
+                    _.dot = _.addSelectCircleEvent[k].call(this, _.mouse, pos);
+                });
+            }
+            if (__options.showLand && _.countries && !_.dot) {
                 _.country = _.countries.features.find(function(f) {
                     return f.geometry.coordinates.find(function(c1) {
                         return polygonContains(c1, pos) || c1.find(function(c2) {
