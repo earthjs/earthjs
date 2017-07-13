c62629362a572af17bf4c8d43f30538632e3db67
wharsojo <wharsojo@gmail.com>
2017-07-13 08:57:57 +0800

    update earthquake

--- example/js/ex/eQuakeApp.js
+++ example/js/ex/eQuakeApp.js
@@ -2,6 +2,7 @@
 // https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson
 // https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
 const eQuakeApp = () => {
+    const _ = {world: null, equake: null};
     return {
         name: 'eQuakeApp',
         urls: [
@@ -10,7 +11,43 @@ const eQuakeApp = () => {
             'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson',
         ],
         onReady(err, world, countryNames, equake) {
-            const features = equake.features.filter(d => d.properties.mag>=3);
+            _.world = world;
+            _.equake = equake;
+            this.eQuakeApp.mag(5);
+            this.hoverCanvas.world(world);
+            this.countrySelectCanvas.world(world);
+            this.worldCanvas.data({world, countryNames});
+        },
+        onInit() {
+            this.register(earthjs.plugins.commonPlugins());
+            this.register(earthjs.plugins.hoverCanvas());
+            this.register(earthjs.plugins.pingsCanvas());
+            this.register(earthjs.plugins.dotsCanvas());
+            this.register(earthjs.plugins.barSvg());
+            this.register(earthjs.plugins.barTooltipSvg());
+            this.register(earthjs.plugins.countrySelectCanvas());
+            this.register(earthjs.plugins.countryTooltipCanvas());
+            this.dotsCanvas.drawTo([1]);
+            this.pingsCanvas.drawTo([1]);
+            this.commonPlugins.addChecker('showPings:Pings:showPings'.split(':'));
+            this.commonPlugins.addChecker('showBars:Bars:showBars'.split(':'));
+            this.commonPlugins.addChecker('showDots:Dots:showDots'.split(':'));
+            var tt = this.barTooltipSvg;
+            tt.onShow = function(d) {
+                var {mag, tsunami, eventtime, place, detail} = d.properties;
+                if (!eventtime) {
+                    d3.json(detail, function(error, data) {
+                        var {eventtime} = data.properties.products.origin[0].properties;
+                        d.properties.eventtime = eventtime;
+                        tt.show({properties: {mag,tsunami,eventtime,place}});
+                    });
+                }
+                return {properties: {mag,tsunami,eventtime,place}};
+            }
+            this._.options.transparent = true;
+        },
+        mag(mag) {
+            const features = _.equake.features.filter(d => d.properties.mag>=mag);
             const maxMag = features.map(d => d.properties.mag).sort(d3.descending)[0];
             const scale = d3.scaleLinear().domain([3, maxMag]).range([0.5, 2]);
             features.forEach(d => {
@@ -26,39 +63,13 @@ const eQuakeApp = () => {
                     strokeStyle: 'rgba(100,0,0,.6)'
                 }
             }
-            this._.options.showLakes = false;
-            this._.options.showCountries = false;
-            this.worldCanvas.data({world, countryNames});
-            this.hoverCanvas.world(world);
             this.barSvg.data(dataMssg);
             this.dotsCanvas.data(dataMssg);
             this.pingsCanvas.data(dataMssg);
+            this.create();
         },
-        onInit() {
-            this.register(earthjs.plugins.commonPlugins());
-            this.register(earthjs.plugins.hoverCanvas());
-            this.register(earthjs.plugins.pingsCanvas());
-            this.register(earthjs.plugins.dotsCanvas());
-            this.register(earthjs.plugins.barSvg());
-            this.register(earthjs.plugins.barTooltipSvg());
-            this.register(earthjs.plugins.countryTooltipCanvas());
-            this.commonPlugins.addChecker(':Pings:showPings'.split(':'));
-            this.commonPlugins.addChecker(':Bars:showBars'.split(':'));
-            this.commonPlugins.addChecker(':Dots:showDots'.split(':'));
-            const tt = this.barTooltipSvg;
-            this.barTooltipSvg.onShow = function(d) {
-                const {mag, tsunami, eventtime, place, detail} = d.properties;
-                if (!eventtime) {
-                    d3.json(detail, function(error, data) {
-                        const {eventtime} = data.properties.products.origin[0].properties;
-                        d.properties.eventtime = eventtime;
-                        tt.show({properties: {mag,tsunami,eventtime,place}});
-                    });
-                }
-                return {properties: {mag,tsunami,eventtime,place}};
-            }
-            this._.options.transparent = true;
-        }
     }
 }
 earthjs.plugins.eQuakeApp = eQuakeApp;
+
+// countryNames
