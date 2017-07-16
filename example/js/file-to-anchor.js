7c909af66ba1414f70dae9e8529040ff58d6c82f
wharsojo <wharsojo@gmail.com>
2017-07-16 12:13:08 +0800

    move files

---
+++ example/js/file-to-anchor.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+var dir = '../'; // your directory
+
+// List all files in a directory in Node.js recursively in a synchronous fashion
+var walkSync = function(dir, filelist) {
+    var fs = fs || require('fs'),
+        files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+        const path = dir+file;
+        const isDir = fs.statSync(path).isDirectory();
+        if (isDir && path.indexOf('/0')>0) {
+            filelist.push(`<h2>${file}</h2>`);
+            filelist = walkSync(path+ '/', filelist);
+        }
+        else {
+            if (file.indexOf('.html')>0) {
+                filelist.push(`<a href="${path.replace('..','')}">${file}</a>`);
+            }
+        }
+    });
+    return filelist;
+};
+
+walkSync(dir).forEach(file => {
+    console.log(file);
+})
+
+
+// var files = fs.readdirSync(dir);
+// files.sort(function(a, b) {
+//    return fs.statSync(dir + a).mtime.getTime() -
+//           fs.statSync(dir + b).mtime.getTime();
+// });
+//
