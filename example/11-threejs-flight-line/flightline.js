3ed44c06519ad732496187a047f503c7403fd6ff
wharsojo <wharsojo@gmail.com>
2017-10-01 02:21:01 +0800

    update examaples

---
+++ example/11-threejs-flight-line/flightline.js
@@ -0,0 +1,10 @@
+const {offsetWidth, offsetHeight} = d3.select('body').node();
+const g = earthjs({width: offsetWidth, height: offsetHeight, padding:5})
+.register(earthjs.plugins.mousePlugin())
+.register(earthjs.plugins.threejsPlugin())
+.register(earthjs.plugins.autorotatePlugin())
+.register(earthjs.plugins.clickCanvas());
+
+const light = g.threejsPlugin.light();
+light.intensity = 0.5;
+light.color.set(0x777777);
