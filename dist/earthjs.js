3e33424dc699e478ba7b8e97defc60494c3e5249
wharsojo <wharsojo@gmail.com>
2017-10-22 12:48:44 +0800

    update zooming for flattenSvg

--- dist/earthjs.js
+++ dist/earthjs.js
@@ -3629,12 +3629,15 @@ var placesSvg = (function (urlPlaces) {
 var flattenSvg = (function () {
     /*eslint no-console: 0 */
     var _ = {};
+    window._flat = _;
 
     function init() {
         var g1 = this._.proj;
         var g2 = d3.geoEquirectangular().scale(this._.options.width / 6.3).translate(this._.center);
         _.g1 = g1;
         _.g2 = g2;
+        _.scale = d3.scaleLinear().domain([47.3, _.g1.scale()]).range([0.0559, 1]);
+        _.scaleG2 = d3.scaleLinear().domain([47.3, _.g1.scale()]).range([0, _.g2.scale()]);
     }
 
     function animation() {
@@ -3648,7 +3651,7 @@ var flattenSvg = (function () {
     }
 
     function interpolatedProjection(a, b) {
-        var px = d3.geoProjection(raw).scale(1);
+        _.px = d3.geoProjection(raw).scale(1);
         var alpha = void 0;
 
         function raw(lamda, pi) {
@@ -3666,12 +3669,12 @@ var flattenSvg = (function () {
                 cb = b.center(),
                 ta = a.translate(),
                 tb = b.translate();
-            px.center([(1 - alpha) * ca[0] + alpha * cb[0], (1 - alpha) * ca[1] + alpha * cb[1]]);
-            px.translate([(1 - alpha) * ta[0] + alpha * tb[0], (1 - alpha) * ta[1] + alpha * tb[1]]);
-            return px;
+            _.px.center([(1 - alpha) * ca[0] + alpha * cb[0], (1 - alpha) * ca[1] + alpha * cb[1]]);
+            _.px.translate([(1 - alpha) * ta[0] + alpha * tb[0], (1 - alpha) * ta[1] + alpha * tb[1]]);
+            return _.px;
         };
         animation.alpha(0);
-        return px;
+        return _.px;
     }
 
     //Rotate to default before animation
@@ -3692,13 +3695,23 @@ var flattenSvg = (function () {
             _.me = me;
             init.call(this);
         },
+        onResize: function onResize() {
+            if (this._.options.map) {
+                var g1 = _.g1.scale();
+                var g2 = _.scale(g1);
+                _.px.scale(g2);
+                _.path.attr('d', this._.path);
+            }
+        },
         toMap: function toMap() {
             var _this2 = this;
 
             defaultRotate.call(this).on('end', function () {
+                _.g2.scale(_.scaleG2(_.g1.scale()));
                 var proj = interpolatedProjection(_.g1, _.g2);
                 _this2._.path = d3.geoPath().projection(proj);
                 animation.call(_this2).on('end', function () {
+                    _.path = _this2._.svg.selectAll('path');
                     _this2._.options.enableCenter = false;
                     _this2._.options.map = true;
                 });
@@ -3708,7 +3721,10 @@ var flattenSvg = (function () {
             var _this3 = this;
 
             this._.rotate([0, 0, 0]);
+            // const scale = _.px.scale();
+            _.g2.scale(_.scaleG2(_.g1.scale()));
             var proj = interpolatedProjection(_.g2, _.g1);
+            // proj.scale(scale);
             this._.path = d3.geoPath().projection(proj);
             animation.call(this).on('end', function () {
                 _this3._.path = d3.geoPath().projection(_.g1);
--- src/svg/flattenSvg.js
+++ src/svg/flattenSvg.js
@@ -1,14 +1,17 @@
 export default () => {
     /*eslint no-console: 0 */
     const _ = {};
+    window._flat = _;
 
     function init() {
         const g1 = this._.proj;
         const g2 = d3.geoEquirectangular()
             .scale(this._.options.width/6.3)
-            .translate(this._.center);
+            .translate(this._.center); 
         _.g1 = g1;
         _.g2 = g2;
+        _.scale = d3.scaleLinear().domain([47.3, _.g1.scale()]).range([0.0559, 1]);
+        _.scaleG2 = d3.scaleLinear().domain([47.3, _.g1.scale()]).range([0, _.g2.scale()]);
     }
 
     function animation() {
@@ -24,7 +27,7 @@ export default () => {
     }
 
     function interpolatedProjection(a, b) {
-        const px = d3.geoProjection(raw).scale(1);
+        _.px = d3.geoProjection(raw).scale(1);
         let alpha;
 
         function raw(lamda, pi) {
@@ -42,16 +45,16 @@ export default () => {
                 cb = b.center(),
                 ta = a.translate(),
                 tb = b.translate();
-            px.center([
+            _.px.center([
                 (1 - alpha) * ca[0] + alpha * cb[0],
                 (1 - alpha) * ca[1] + alpha * cb[1]]);
-            px.translate([
+            _.px.translate([
                 (1 - alpha) * ta[0] + alpha * tb[0],
                 (1 - alpha) * ta[1] + alpha * tb[1]]);
-            return px;
+            return _.px;
         };
         animation.alpha(0);
-        return px;
+        return _.px;
     }
 
     //Rotate to default before animation
@@ -74,11 +77,21 @@ export default () => {
             _.me = me;
             init.call(this);
         },
+        onResize() {
+            if (this._.options.map) {
+                const g1 = _.g1.scale();
+                const g2 = _.scale(g1);
+                _.px.scale(g2);
+                _.path.attr('d', this._.path);    
+            }
+        },
         toMap() {
             defaultRotate.call(this).on('end', () => {
+                _.g2.scale(_.scaleG2(_.g1.scale()));
                 const proj = interpolatedProjection(_.g1, _.g2);
                 this._.path = d3.geoPath().projection(proj);
                 animation.call(this).on('end', () => {
+                    _.path = this._.svg.selectAll('path');
                     this._.options.enableCenter = false;
                     this._.options.map = true;
                 })
@@ -86,7 +99,10 @@ export default () => {
         },
         toGlobe() {
             this._.rotate([0,0,0]);
+            // const scale = _.px.scale();
+            _.g2.scale(_.scaleG2(_.g1.scale()));
             const proj = interpolatedProjection(_.g2, _.g1);
+            // proj.scale(scale);
             this._.path = d3.geoPath().projection(proj);
             animation.call(this).on('end', () => {
                 this._.path = d3.geoPath().projection(_.g1);
